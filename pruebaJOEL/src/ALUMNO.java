2934e5c2a5b384b922ae55c638d41d31e61bbb48
joel <joel@192.168.1.36>
2015-03-25 06:56:26 -0700

    he creado la clase alumno

---
+++ pruebaJOEL/src/ALUMNO.java
@@ -0,0 +1,24 @@
+
+public class ALUMNO {
+     int dni;
+     int codigo_matri;
+     
+     public ALUMNO(){}
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public int getCodigo_matri() {
+        return codigo_matri;
+    }
+
+    public void setCodigo_matri(int codigo_matri) {
+        this.codigo_matri = codigo_matri;
+    }
+     
+}
